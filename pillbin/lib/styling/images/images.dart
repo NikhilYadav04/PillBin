d36eefcf94eb395262651dd17c1b3367092cbb4e
NikhilYadav04 <byadav1723@gmail.com>
2024-12-23 01:00:20 +0530

    Initialized Structure

---
+++ pillbin/lib/styling/images/images.dart
@@ -0,0 +1,13 @@
+class Images{
+  Images._();
+
+  static const BG_Video = "assets/BG_Video.mp4";
+
+  static const Image_2 = "assets/Image_2.webp";
+  static const Image_3 = "assets/Image_3.webp";
+  static const Image_4 = "assets/Image_4.jpg";
+  static const Image_5 = "assets/Image_5.png";
+  static const Image_6 = "assets/Image_6.png";
+
+  static const Logo = "assets/Logo.png";
+}
